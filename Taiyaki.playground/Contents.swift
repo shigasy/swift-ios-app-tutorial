7c2a95841b931575c8ed9f195ccc35236a9950a4
shigasy <kxx555u.u@gmail.com>
2020-08-22 20:09:08 +0900

    add: structとclassの違い, structは値型であり，比較的単純なデータ値をカプセル化するためにある．classはそれ以外で使う．classは参照型

---
+++ Taiyaki.playground/Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+struct Taiyaki {
+    var nakami = "あんこ"
+    func sayNakami() {
+        print("中身は\(nakami)です")
+    }
+}
+//class Bar {
+//    var a: UInt8
+//    var b: UInt8
+//    init(a: UInt8, b: UInt8) { self.a = a; self.b = b }
+//}
+//let bar = Bar(a: 3, b: 127)
+//bar.a = 5 // OK
+
+// 構造体だから，値型であり，ERRORになる．
+// classだったら，参照型だから，letでも書き換えられる．？
+// structの目的は比較的単純なデータ値をカプセル化すること
+// メモリ云々はSwiftにCopy on Writeという「値型のオブジェクトは、代入先で書き込まれた時に初めてコピーされる」機能があるらしく，そこまで気にしなくて良いらしい．
+// https://qiita.com/narukun/items/24d9e68ff965f8da09ad
+// classだと以下のvarがletにできる
+var taiyaki = Taiyaki()
+taiyaki.nakami = "クリーム"
+// 値がコピーされて，コピーされたインスタンスのプロパティを変更されて，コピーされたインスタンスのメソッドを使っているから変更される．
+taiyaki.sayNakami()
+
+// class使って，参照型でもlet使って instance.propaty = 'a'と出来るだろうけど，どこで変更したか分からなくなりそうなので，セッターか何か使ったほうが良さそうだな〜，わからん．正直初期化以外で上書きしたくない．
+
+

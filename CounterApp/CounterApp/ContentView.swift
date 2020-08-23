f1b90756c3d32b88624399fd6f53bc76b5e7b5e4
shigasy <kxx555u.u@gmail.com>
2020-08-23 18:04:08 +0900

    add: クロージャ(他言語で言うラムダに似ている)の書き方 トレイリングクロージャの特徴 最後の引数がクロージャの場合に、クロージャを()の外に出して、ラベルを省略できる。そのための最後のブロック｛｝ 唯一の引数がクロージャの場合に、クロージャを()の外に出してラベルと()を省略できる。

---
+++ CounterApp/CounterApp/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  CounterApp
+//
+//  Created by Keita Shiga on 2020/08/23.
+//  Copyright © 2020 Keita Shiga. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var number = 0
+    var body: some View {
+        VStack {
+            Text("\(number)")
+            
+            // 短いクロージャの書き方
+            Button(action: {self.number += 1}) {
+                
+            // 本来の無名関数（クロージャ）の書き方
+//            Button(action: {() -> Void in self.number += 1}) {
+                Text("カウント")
+            }
+            
+            // 末尾がクロージャの場合、省略できる（トレイリングクロージャ）。しない場合は
+            // ドキュメントを見ると init(action: () -> Void, label: () -> Label) と書いてある
+            Button(action: {self.number += 1}, label: {Text("カウント")})
+            
+            
+//            func count() {
+//                self.number += 1
+//            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

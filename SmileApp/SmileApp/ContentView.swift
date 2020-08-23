df4114c5927ceefe863fa61ded8d8d9789c6d2fd
shigasy <kxx555u.u@gmail.com>
2020-08-23 14:39:04 +0900

    add: Image構造体のイニシャライザ init(systemName: Strin)を指定して，初期化している．構造体に含まれる全てのプロパティに初期値が設定されている場合に，init()を書かなくてもよいが，暗黙的に追加してくれている

---
+++ SmileApp/SmileApp/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  SmileApp
+//
+//  Created by Keita Shiga on 2020/08/23.
+//  Copyright © 2020 Keita Shiga. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Image(systemName: "smiley")
+            // 画面サイズを変更するmodifier
+            .resizable()
+            // 縦横比を適正に設定
+            .foregroundColor(.green)
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            .scaledToFit()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

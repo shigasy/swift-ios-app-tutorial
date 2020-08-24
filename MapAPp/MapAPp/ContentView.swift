2c0fdb2cacc54e3eb59696be923af744ad9e07a8
shigasy <kxx555u.u@gmail.com>
2020-08-24 19:45:35 +0900

    add: mapkitを追加してmap表示

---
+++ MapAPp/MapAPp/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  MapAPp
+//
+//  Created by Keita Shiga on 2020/08/24.
+//  Copyright © 2020 Keita Shiga. All rights reserved.
+//
+
+import MapKit
+import SwiftUI
+
+struct MapView: UIViewRepresentable {
+    func makeUIView(context: Context) -> MKMapView {
+        return MKMapView()
+    }
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        MapView().edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

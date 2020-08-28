56cbcf0390f30d0288258027cc4d28cdde575b56
shigasy <kxx555u.u@gmail.com>
2020-08-28 18:05:56 +0900

    add: NSObjectクラスを継承して、その他2つのプロトコルを批准 理由は、2つのプロトコルがNSObjectProtocolを批准しており、そのためのメソッドやプロパティを継承するため 2つのプロトコルはカメラ機能を実装するためにUIKitを使った場合に指定されているプロトコル 最低限アプリが動くために必要なメソッドやプロパティは必須だが、任意のメソッドやプロパティも存在する それでもプロトコルを指定する理由は、そのプロトコルと同じメソッドなどを定義すれば、引数で値を受け取れるように設計されているから

--- CameraApp/CameraApp/Coordinator.swift
+++ CameraApp/CameraApp/Coordinator.swift
@@ -8,6 +8,22 @@
 
 import SwiftUI
 
+// NSObjectクラスを継承
+// UINavigationConrtollerDelegateとUIImagePickerCOntrollerDelegateはプロトコル
+// UINavigationConrtollerDelegateとUIImagePickerCOntrollerDelegateはNSObjectProtocolプロトコルを継承している。
+//  UINavigationConrtollerDelegateとUIImagePickerCOntrollerDelegateを批准するために必要となるプロパティやメソッドに加えて、NSObjectProtocolプロトコルを批准するために必要となるプロパティやメソッドも定義しなければならない。
+//  NSObjectProtocolプロトコルに批准するために必要なプロパティやメソッドを書くと複雑になるため、NSObjectクラスを継承することで、それらのプロパティやメソッドを継承する。
+// クラスを継承することで多くの機能を利用できるようになる。
+// クラスには1つの親しか指定でいないが、プロトコルは複数指定して、批准できる
+//
+// 2つのプロトコルはoptionalが付いていて、プロパティやメソッドの実装は任意
+// 任意にも関わらず、プロトコルを指定する理由はプロトコルを批准させると、引数を使って値を取得できるようになっているから。
+// 例えば、imagePickerControllerメソッドの実装はオプションで、ユーザーが写真を撮り終えたタイミングでiOSが自動的に呼び出すメソッド
+// 引数にはユーザーが撮影した画像が含まれる
+// なくても、最低限アプリが動くため、オプションになっている（カメラアプリとして画像を保存したり編集したりできないのはカメラアプリとして失格だが。）
+// そのメソッドを実装しないとアプリが動作しない場合は、必須のメソッドとなっている
+
+// プロトコルはUIKitフレームワークを使ってカメラ機能を実装する場合はほぼ強制的にこの2つのプロトコルを批准させることになる。Apple社が定めたルール
 class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     var parent:ImagePicker
     

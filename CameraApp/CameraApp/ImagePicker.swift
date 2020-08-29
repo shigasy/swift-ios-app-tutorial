49eee229d7c9a9b3f647baeebdf676c77f3b27c5
shigasy <kxx555u.u@gmail.com>
2020-08-29 23:49:45 +0900

    add: delegateの説明

--- CameraApp/CameraApp/ImagePicker.swift
+++ CameraApp/CameraApp/ImagePicker.swift
@@ -13,13 +13,21 @@ import SwiftUI
 // Apple DeveloperとかApple Developer Forumsとかを見る
 struct ImagePicker: UIViewControllerRepresentable {
     
+    // UIViewControllerRepresentableプロトコルに宣言されているメソッド
+    // UIKitフレームワークの中のUI部品（今回はUIImagePickerController）の調整役となるインスタンスを作るためのメソッド
+    // その調整役がCoordinatorクラス
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
     
     func makeUIViewController(context: Context) -> UIImagePickerController {
+        // UIImagePickerControllerというUIKitフレームワークのUI部品をインスタンス化
         let picker = UIImagePickerController()
+        // 画像の取得方法をカメラ
         picker.sourceType = .camera
+        // delegateとは直訳すると代理人という意味
+        // あるクラスだけでは処理できない命令を、そのクラスの代わりに行うクラスのことをデリゲートと呼ぶ
+        // 全てのクラスが別のクラスのデリゲートになれる
         picker.delegate = context.coordinator
         return picker
     }

cf52c6a10bbcd1b5ad56f4357e5bfde1b71c8899
shigasy <kxx555u.u@gmail.com>
2020-09-02 18:42:24 +0900

    initl LocalizationApp

---
+++ LocalizationApp/LocalizationApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LocalizationApp
+//
+//  Created by Keita Shiga on 2020/09/02.
+//  Copyright © 2020 Keita Shiga. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
